33580bbf22395968ee88b52dda7f77ff377b7dae
Hafiizh Taufiqul Hakim <2012500720@student.budiluhur.ac.id>
2024-05-26 20:18:28 +0700

    Update file

--- app.py
+++ app.py
@@ -333,7 +333,7 @@ elif page == "FAQ":
         - Curricular_units_1st_sem_grade
         - Curricular_units_2nd_sem_enrolled
         - Curricular_units_2nd_sem_approved
-        - Curricular_units_2nd_sem_grade
+        - Curricular_units_2nd_sem_grade   
         """)
 
     with st.expander("Bagaimana cara menggunakan aplikasi ini?"):
